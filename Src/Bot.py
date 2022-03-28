73c26f6c5fe7a356227f005707ccd4d4ddab54bb
koikalinin <100307217+koikalinin@users.noreply.github.com>
2022-03-28 17:43:22 +0300

    Add files via upload

--- Src/Bot.py
+++ Src/Bot.py
@@ -1,28 +1,32 @@
 from Secret import teletele
-from Parsing import parsing, showtext
+#from Parsing import parsing, showtext
+from ParsingHeadless import parsing, showtext
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import (
     CallbackQueryHandler,
     Updater,
     CommandHandler,
-    CallbackContext,
+    CallbackContext
 )
 
 
 updater = Updater(token=teletele, use_context=True)
 dispatcher = updater.dispatcher
 
-def hello(update, context):
+def start(update, context):
+    print(f'/start command from {update.effective_chat.id}')
     context.bot.send_message(chat_id=update.effective_chat.id, text=
-    '''Greetings, mortal. \n By the will of the Chaos Gods I will show you the latest Total War Warhammer 3 posts. 
-     Use /showlinks and /showtext too see what we have'''
+    '''Greetings! \n   Use /links and /text too see the latest Total War Warhammer 3 posts. \n 
+    Due to code restrictions, it is advised to use /links before /text command.'''
                              )
 
-hello_handler = CommandHandler('hello', hello)
-dispatcher.add_handler(hello_handler)
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
 result = []
 def parcecommand(update, context):
+    #composes links into readable list
+    print(f'/links command from {update.effective_chat.id}')
     global result
     text = parsing()
     result = text
@@ -33,11 +37,14 @@ def parcecommand(update, context):
         i+=1
     context.bot.send_message(chat_id=update.effective_chat.id, text=textstring)
 
-
-showlinks_handler = CommandHandler('showlinks', parcecommand)
-dispatcher.add_handler(showlinks_handler)
+links_handler = CommandHandler('links', parcecommand)
+dispatcher.add_handler(links_handler)
 
 def gettext(update, context):
+    #creates keybord and sends data to query when user clicks on button
+    #user choses a link from keyboard and gets parsed data
+    chat_id = update.effective_chat.id
+    print(f'/text command from {chat_id}')
     keyboard = [
         [
             InlineKeyboardButton("First link", callback_data='1'),
@@ -47,25 +54,24 @@ def gettext(update, context):
             InlineKeyboardButton("Third link", callback_data='3'),
             InlineKeyboardButton("Forth link", callback_data='4')
         ],
-    ] #когда на тебя нажмут, отправь в query callback_data
+    ]
 
-    reply_markup = InlineKeyboardMarkup(keyboard) #создает окошко с кнопками
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text('Choose one:', reply_markup=reply_markup) #добавляет окошко на экран с текстом
+    update.message.reply_text('Choose one:', reply_markup=reply_markup)
 
-showtext_handler = CommandHandler('showtext', gettext)
-dispatcher.add_handler(showtext_handler)
+text_handler = CommandHandler('text', gettext)
+dispatcher.add_handler(text_handler)
 
-def button(update: Update, context: CallbackContext) -> None: #мы получаем инфу о кнопке и
-    query = update.callback_query #здесь получаем инфу о том, какая кнопка нажата
+def button(update: Update, context: CallbackContext) -> None:
+    #updates query info from pressed buttons and sends a respond
+    query = update.callback_query
 
 
     query.answer()
-    #print(result[int(query.data)-1])
-    #print(showtext(result[int(query.data)-1]))
-    query.edit_message_text(text=f"You chose: {query.data}") #передает инфу записанную в квери.дата
+    query.edit_message_text(text=f"You chose: {query.data}")
     query.edit_message_text(text=showtext(result[int(query.data)-1]))
 
 
-updater.dispatcher.add_handler(CallbackQueryHandler(button)) #тут мы говорим - свяжи нажатие на кнопку(любую) с функцией Button
+updater.dispatcher.add_handler(CallbackQueryHandler(button)) #links any button with button() function
 updater.start_polling()
